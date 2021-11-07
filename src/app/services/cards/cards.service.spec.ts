d456a66db71d7bfbca15aebc42128e9a523966b3
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-07 23:14:45 +0300

    cards service tests and get requests

--- src/app/services/cards/cards.service.spec.ts
+++ src/app/services/cards/cards.service.spec.ts
@@ -1,16 +1,66 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CardsService } from './cards.service';
 
 describe('CardsService', () => {
-  let service: CardsService;
+  let cardService: CardsService;
+  let httpTestingController: HttpTestingController;
+  let succeeded = false;
+  let baseUrl = 'https://api.magicthegathering.io/v1/sets';
+  const mockResponse = {
+    "cards": [
+      {
+        "name": "Meandering Towershell",
+        "manaCost": "{3}{G}{G}",
+        "cmc": 5,
+        "colors": [
+          "Green"
+        ],
+        "type": "Creature — Turtle",
+        "types": [
+          "Creature"
+        ],
+        "subtypes": [
+          "Turtle"
+        ],
+      }
+    ]
+  }
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CardsService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CardsService],
+    });
+    cardService = TestBed.inject(CardsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(cardService).toBeTruthy();
   });
+
+  // Test get cards based on sets
+  describe('Sets API Tests', () => {
+    it('should get cards  based on setId', () => {
+      let id = "KTK";
+      cardService.getCards(id).subscribe((resp) => {
+        succeeded = true;
+        expect(resp).not.toBe({ empty: true });
+        expect(resp).toEqual(mockResponse, 'should return expected results'), fail;
+        expect(succeeded).toBeTrue();
+      });
+
+      const req = httpTestingController.expectOne(baseUrl + '/' + id + '/booster');
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(mockResponse);
+
+      httpTestingController.verify();
+    });
+  })
+
 });
--- src/app/services/cards/cards.service.ts
+++ src/app/services/cards/cards.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardsService {
 
-  constructor() { }
+  constructor(private apiService:ApiService) { }
+
+  getCards(setId){
+    return this.apiService.get('sets/'+ setId +'/booster');
+  }
+
 }
