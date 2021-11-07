0ca2b00e664823ebb6e67fcda64041e531065dd0
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-07 10:24:15 +0300

    spec file tests

--- src/app/services/sets/sets.service.spec.ts
+++ src/app/services/sets/sets.service.spec.ts
@@ -1,16 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { SetsService } from './sets.service';
 
 describe('SetsService', () => {
-  let service: SetsService;
+  let setService: SetsService;
+ 
+  
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SetsService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
+    setService = TestBed.inject(SetsService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(setService).toBeTruthy();
+  });
+
+  it('should make a get request', () => {
+    // let sets =[];
+    let sets = [];
+    setService.getSets().subscribe((resp:any)=>{
+      sets.push(resp)
+      expect(resp).toEqual(sets, 'should return expected sets'),
+        fail
+
+    });
+    // expect(sets.length).toBeGreaterThan(0);
   });
 });
