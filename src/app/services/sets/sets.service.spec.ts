256ddafbd680e559d36a5a26a97393843a2fdd38
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-07 22:46:33 +0300

    updated unit test for sets

--- src/app/services/sets/sets.service.spec.ts
+++ src/app/services/sets/sets.service.spec.ts
@@ -6,6 +6,37 @@ import { SetsService } from './sets.service';
 describe('SetsService', () => {
   let setService: SetsService;
   let httpTestingController: HttpTestingController;
+  let succeeded = false;
+  let baseUrl = 'https://api.magicthegathering.io/v1/sets';
+
+  const mockResponse = {
+    "sets": [
+      {
+        "code": "10E",
+        "name": "Tenth Edition",
+        "type": "core",
+        "booster": [
+          "rare",
+          "uncommon",
+          "uncommon",
+          "uncommon",
+          "common",
+          "common",
+          "common",
+          "common",
+          "common",
+          "common",
+          "common",
+          "common",
+          "common",
+          "common"
+        ],
+        "releaseDate": "2007-07-13",
+        "block": "Core Set",
+        "onlineOnly": false
+      },
+    ]
+  }
 
 
   beforeEach(() => {
@@ -24,56 +55,40 @@ describe('SetsService', () => {
   });
 
   describe('Sets API Tests', () => {
+    
+    // Test get all sets
     it('should make get request for sets', () => {
-      let succeeded = false;
-
-      const mockResponse = {
-        "sets": [
-          {
-            "code": "10E",
-            "name": "Tenth Edition",
-            "type": "core",
-            "booster": [
-              "rare",
-              "uncommon",
-              "uncommon",
-              "uncommon",
-              "common",
-              "common",
-              "common",
-              "common",
-              "common",
-              "common",
-              "common",
-              "common",
-              "common",
-              "common"
-            ],
-            "releaseDate": "2007-07-13",
-            "block": "Core Set",
-            "onlineOnly": false
-          },
-        ]
-      }
-
       setService.getSets().subscribe((resp) => {
         succeeded = true;
-        expect(resp).not.toBe({empty:true});
-        expect(resp).toEqual(mockResponse, 'should return expected results'),fail;
+        expect(resp).not.toBe({ empty: true });
+        expect(resp).toEqual(mockResponse, 'should return expected results'), fail;
         expect(succeeded).toBeTrue();
       });
 
-      const req = httpTestingController.expectOne('https://api.magicthegathering.io/v1/sets');
+      const req = httpTestingController.expectOne(baseUrl);
       expect(req.request.method).toEqual('GET');
-  
 
       req.flush(mockResponse);
 
       httpTestingController.verify();
+    });
 
+    // Test get single set based on Id
+    it('should get a single set based on Id', () => {
+      let id = "KTK";
+      setService.getSingleSet(id).subscribe((resp) => {
+        succeeded = true;
+        expect(resp).not.toBe({ empty: true });
+        expect(resp).toEqual(mockResponse, 'should return expected results'), fail;
+        expect(succeeded).toBeTrue();
+      });
 
+      const req = httpTestingController.expectOne(baseUrl + '/' + id);
+      expect(req.request.method).toEqual('GET');
 
+      req.flush(mockResponse);
 
+      httpTestingController.verify();
     });
 
   })
