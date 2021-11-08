23844e0b72dea063bcddf4eb0518576f5c5bcbab
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-08 23:36:44 +0300

    sets test

--- src/app/components/sets/sets.component.spec.ts
+++ src/app/components/sets/sets.component.spec.ts
@@ -6,25 +6,57 @@ import { SetsComponent } from './sets.component';
 describe('SetsComponent', () => {
   let component: SetsComponent;
   let fixture: ComponentFixture<SetsComponent>;
-  let h6: HTMLElement;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SetsComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SetsComponent],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(SetsComponent);
-    component = fixture.componentInstance;
-    h6 = fixture.nativeElement.querySelector('h6')
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(SetsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('no title in the DOM after createComponent()', () => {
-    expect(h6.textContent).toEqual('');
+  it('should correctly render the passed @Input value', () => {
+    component.set = {
+      code: '10E',
+      name: 'Tenth Edition',
+      type: 'core',
+      booster: [
+        'rare',
+        'uncommon',
+        'uncommon',
+        'uncommon',
+        'common',
+        'common',
+        'common',
+        'common',
+        'common',
+        'common',
+        'common',
+        'common',
+        'common',
+        'common',
+      ],
+      releaseDate: '2007-07-13',
+      block: 'Core Set',
+      onlineOnly: false,
+    }; // 1
+    fixture.detectChanges(); // 2
+    const compiled = fixture.debugElement.nativeElement; // 2
+    expect(compiled.querySelector('h5').textContent).toBe('Tenth Edition');
+    expect(compiled.querySelector('h6').textContent).toBe('13.07.2007');
+    expect(compiled.querySelector('h6').textContent).toBe('13.07.2007'); // 3
+    expect(compiled.querySelector('p').textContent).toBe('Core Set | core'); 
+    // 3
+     // 3
+     // 3
   });
 });
