0435b17537a0461661ccf17d3dd771ff134782c8
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-17 08:22:55 +0300

    home page tests update

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -5,6 +5,11 @@ import { By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 import { MockComponent } from 'ng-mocks';
 import { SetsComponent } from 'src/app/components/sets/sets.component';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+}
+  from '@angular/platform-browser-dynamic/testing';
 
 
 import { HomePage } from './home.page';
@@ -16,7 +21,9 @@ describe('HomePage', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePage, MockComponent(SetsComponent)],
+      declarations: [HomePage,
+        MockComponent(SetsComponent)
+      ],
 
       imports: [IonicModule.forRoot(), HttpClientTestingModule,]
     }).compileComponents();
@@ -25,79 +32,82 @@ describe('HomePage', () => {
     homeComponent = fixture.componentInstance;
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(homeComponent).toBeTruthy();
-  });
-
-  function findComponent<T>(fixture: ComponentFixture<T>, selector: string,): DebugElement {
-    return fixture.debugElement.query(By.css(selector));
-  }
-
-  // function childComponents(): ChildComponent[] {
-  //   return fixture.debugElement
-  //     .queryAll(By.directive(ChildComponent))
-  //     .map(el => el.componentInstance);
-  // }
-
-  it('renders an independent sets component', () => {
+    
     homeComponent.sets = [{
       "code": "10E",
       "name": "Tenth Edition",
       "type": "core",
       "booster": [
-          "rare",
-          "uncommon",
-          "uncommon",
-          "uncommon",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common"
+        "rare",
+        "uncommon",
+        "uncommon",
+        "uncommon",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common"
       ],
       "releaseDate": "2007-07-13",
       "block": "Core Set",
       "onlineOnly": false
-  },
-  {
+    },
+    {
       "code": "2ED",
       "name": "Unlimited Edition",
       "type": "core",
       "booster": [
-          "rare",
-          "uncommon",
-          "uncommon",
-          "uncommon",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common",
-          "common"
+        "rare",
+        "uncommon",
+        "uncommon",
+        "uncommon",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common",
+        "common"
       ],
       "releaseDate": "1993-12-01",
       "block": "Core Set",
       "onlineOnly": false
-  }]; // 1
-    // const compiled = fixture.debugElement.nativeElement;
-    // fixture.detectChanges();
-   const sets = findComponent(fixture, 'app-sets');
-    expect(sets).toBeTruthy();
+    }];
+  }));
 
+  //find component within the parent DOM
+  function findComponent<T>(fixture: ComponentFixture<T>, selector: string,): DebugElement {
+    fixture.detectChanges();
+    return fixture.debugElement.query(By.css(selector));
+  }
+
+  //find component within the parent Child
+  function childComponents(): SetsComponent[] {
+    fixture.detectChanges();
+    return fixture.debugElement.queryAll(By.directive(SetsComponent)).map(el => el.componentInstance);
+  }
+
+  it('should create', () => {
+    expect(homeComponent).toBeTruthy();
+    const counter = findComponent(fixture, 'app-sets');
+    expect(counter.properties).toBeTruthy();
   });
 
-  // helper function to query all the ChildComponents
+  it('should create one child component for each child', () => {
+    expect(childComponents().length).toEqual(homeComponent.sets.length);
+  });
+
+  it('should set to the name of the set Input', () => {
+    expect(childComponents().map(c => c.set)).toEqual(homeComponent.sets);
+  });
 
 });
