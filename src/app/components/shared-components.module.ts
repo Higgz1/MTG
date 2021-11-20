7d71aa3ff8ffaf387c7dc9486a7b865f7abc1337
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-20 23:11:44 +0300

    Added Header Component

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -4,13 +4,14 @@ import { IonicModule } from '@ionic/angular';
 import { SetsComponent } from './sets/sets.component';
 import { SingleCardComponent } from './single-card/single-card.component';
 import { CardsComponent } from './cards/cards.component';
+import { HeaderComponent } from './header/header.component';
  
 @NgModule({
-  declarations: [SetsComponent,SingleCardComponent,CardsComponent],
+  declarations: [SetsComponent,SingleCardComponent,CardsComponent,HeaderComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [SetsComponent,SingleCardComponent,CardsComponent]
+  exports: [SetsComponent,SingleCardComponent,CardsComponent,HeaderComponent]
 })
 export class SharedComponentsModule { }
