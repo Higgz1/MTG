ac8aae0faae11c6fb7fbc3e7fa3aff6d07864cec
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-12-02 18:17:18 +0300

    updated sets component tests

--- src/app/components/sets/sets.component.spec.ts
+++ src/app/components/sets/sets.component.spec.ts
@@ -5,79 +5,96 @@ import { IonicModule } from '@ionic/angular';
 
 import { SetsComponent } from './sets.component';
 
-xdescribe('SetsComponent', () => {
+describe('SetsComponent', () => {
   let setsComponent: SetsComponent;
   let fixture: ComponentFixture<SetsComponent>;
+  let el: DebugElement;
+
+  const mockResponse = {
+    "object": "list",
+    "has_more": false,
+    "data": [
+      {
+        "object": "set",
+        "id": "78a7f4da-4838-4011-9f58-de8020d1fd2d",
+        "code": "cc2",
+        "tcgplayer_id": 2905,
+        "name": "Commander Collection: Black",
+        "uri": "https://api.scryfall.com/sets/78a7f4da-4838-4011-9f58-de8020d1fd2d",
+        "scryfall_uri": "https://scryfall.com/sets/cc2",
+        "search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Acc2&unique=prints",
+        "released_at": "2022-01-28",
+        "set_type": "from_the_vault",
+        "card_count": 9,
+        "digital": false,
+        "nonfoil_only": false,
+        "foil_only": false,
+        "icon_svg_uri": "https://c2.scryfall.com/file/scryfall-symbols/sets/cc2.svg?1636952400"
+      }, {
+        "object": "set",
+        "id": "a4a0db50-8826-4e73-833c-3fd934375f96",
+        "code": "aer",
+        "mtgo_code": "aer",
+        "arena_code": "aer",
+        "tcgplayer_id": 1857,
+        "name": "Aether Revolt",
+        "uri": "https://api.scryfall.com/sets/a4a0db50-8826-4e73-833c-3fd934375f96",
+        "scryfall_uri": "https://scryfall.com/sets/aer",
+        "search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints",
+        "released_at": "2017-01-20",
+        "set_type": "expansion",
+        "card_count": 194,
+        "printed_size": 184,
+        "digital": false,
+        "nonfoil_only": false,
+        "foil_only": false,
+        "block_code": "kld",
+        "block": "Kaladesh",
+        "icon_svg_uri": "https://c2.scryfall.com/file/scryfall-symbols/sets/aer.svg?1637557200"
+      }
+    ]
+  }
+
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SetsComponent],
-      schemas: [NO_ERRORS_SCHEMA],
       imports: [IonicModule.forRoot()],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SetsComponent);
     setsComponent = fixture.componentInstance;
-    fixture.detectChanges();
+    el = fixture.debugElement;
+  }));
 
-    setsComponent.set = {
-      "code": "10E",
-      "name": "Tenth Edition",
-      "type": "core",
-      "booster": [
-        "rare",
-        "uncommon",
-        "uncommon",
-        "uncommon",
-        "common",
-        "common",
-        "common",
-        "common",
-        "common",
-        "common",
-        "common",
-        "common",
-        "common",
-        "common"
-      ],
-      "releaseDate": "2007-07-13",
-      "block": "Core Set",
-      "onlineOnly": false
-    }
-
-    fixture.detectChanges();
-  });
-
-  function findComponent<T>(fixture: ComponentFixture<T>, selector: string,): DebugElement {
-    fixture.detectChanges();
-    return fixture.debugElement.query(By.css(selector));
-  }
 
   it('should create', () => {
-    const name = findComponent(fixture,'.h5');
-    expect(name).toBeTruthy();
+    setsComponent.set = mockResponse.data;
+    fixture.detectChanges();
 
+    const sets = el.queryAll(By.css('ion-card'));
+    expect(sets).toBeTruthy("no sets coming through");
+    expect(sets.length).toBe(1);
   });
 
+  it('should display the correct Set Card content', () => {
 
-  // it('should correctly render the passed @Input value', () => {
-  //   expect(setsComponent.set).toBe(undefined);
+    setsComponent.set = mockResponse.data[0];
+    fixture.detectChanges();
 
-  //   // const compiled = fixture.debugElement.nativeElement;//  
-  //   // fixture.detectChanges();
-  //   // expect(compiled.querySelector('.set-name').textContent).toEqual(setsComponent.set.name);
+    const setCard = el.queryAll(By.css('ion-card')),
+      name = setCard[0].query(By.css('.set-name')),
+      first_badge = setCard[0].query(By.css('.first-badge')),
+      second_badge = setCard[0].query(By.css('.second-badge')),
+      icon = setCard[0].query(By.css('ion-img'));
 
-  //   // expect(compiled.querySelector('h5').textContent).toContain(component.set.name);
-  //   // expect(compiled.querySelector('.name-circle').textContent).toBe(' T ');
-  //   // expect(compiled.querySelector('h5').textContent).toBe(component.set.name);
-  //   // expect(compiled.querySelector('h6').textContent).toBe('13.07.2007');
-  //   // expect(compiled.querySelector('h6').textContent).toBe('13.07.2007'); // 3
-  //   // expect(compiled.querySelector('p').textContent).toBe('Core Set | core | Jul.2007');
-  //   // expect(compiled.querySelector('.name-circle').style.background).toBe('rgb(46, 175, 159)'); 
+    expect(setCard).toBeTruthy("Could not find set card");
+    expect(name.nativeElement.textContent).toBe(setsComponent.set.name);
+    expect(first_badge.nativeElement.textContent).toBe(' '+setsComponent.set.card_count + ' cards ');
+    expect(second_badge.nativeElement.textContent).toBe(' '+setsComponent.set.set_type + ' ');
+    expect(icon.nativeElement.src).toBe(setsComponent.set.icon_svg_uri);
 
-  // });
+  });
 
 
 });
