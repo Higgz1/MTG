a4dc8415422ac7b5e91a4a3b3d223e2b9c266234
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-12-19 21:26:15 +0300

    search functionality for the home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -8,12 +8,14 @@ import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
 import { SharedComponentsModule } from 'src/app/components/shared-components.module';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    Ng2SearchPipeModule,
     SharedComponentsModule,
     HomePageRoutingModule
   ],
