1de407eb2ee07515701a1d628cb52cd1d42ca42b
Bryan Higgz <bryan.kahiga@gmail.com>
2022-01-26 05:38:42 +0300

    placeholder image should image not load

--- src/app/components/sets/sets.component.ts
+++ src/app/components/sets/sets.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SetsComponent implements OnInit {
   @Input() set: any;
+  noImage:boolean = false;
+  onSuccess: boolean =false;
 
   constructor() { }
 
@@ -14,5 +16,20 @@ export class SetsComponent implements OnInit {
 
   getCards(code) {
     // console.log(code);
+  }
+
+  loadError(event){
+    this.noImage = true;
+    this.set.icon_svg_uri = 'https://c2.scryfall.com/file/scryfall-symbols/sets/default.svg?1643000400';
+    // console.log("ERROR",this.set);
+  }
+
+  willLoad(event){
+    // console.log("WILL LOAD",event);
+  }
+
+  loadSuccess(event){
+    // console.log("SUCCESS");
+    // this.onSuccess = true;
   }
 }
