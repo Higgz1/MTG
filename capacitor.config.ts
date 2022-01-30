f33f22e42ec789446665dab3a7b4488c7b17a881
Bryan Higgz <bryan.kahiga@gmail.com>
2022-01-30 13:53:45 +0300

    renamed appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'higgz.mtg.project',
   appName: 'MTG',
   webDir: 'www',
   bundledWebRuntime: false
