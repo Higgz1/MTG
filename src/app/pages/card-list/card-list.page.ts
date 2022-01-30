3b1b7ee0160315b0cf57e4e3bdf075a186461621
Bryan Higgz <bryan.kahiga@gmail.com>
2022-01-30 14:08:37 +0300

    card list loading indicator

--- src/app/pages/card-list/card-list.page.ts
+++ src/app/pages/card-list/card-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { CardsService } from 'src/app/services/cards/cards.service';
 
 @Component({
@@ -13,7 +14,9 @@ export class CardListPage implements OnInit {
 
   constructor(
     private cardsService: CardsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public loadingController: LoadingController
+
   ) {
     this.cardUri = JSON.parse(this.route.snapshot.paramMap.get('uri'));
   }
@@ -24,13 +27,25 @@ export class CardListPage implements OnInit {
     this.getCards();
   }
 
-  getCards() {
+  async getCards() {
+
+    const loading = await this.loadingController.create({
+      spinner: 'crescent',
+      message: 'Please wait...',
+      // message: 'Click the backdrop to dismiss early...',
+      translucent: true,
+      // cssClass: 'custom-class custom-loading',
+      backdropDismiss: true
+    });
+    await loading.present();
 
     this.cardsService.getCards(this.cardUri).subscribe((cards: any) => {
       this.cards = cards.data;
       //adding index property to cards list
       for (const c of this.cards) {
         c.index = this.cards.indexOf(c);
+        loading.dismiss();
+
       }
       console.log('cards', this.cards);
     });
